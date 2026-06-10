4de60d8ac07843245f674e3da7a321cca837f82b
qilan0v0 <qilan0v0@gmail.com>
2026-06-11 03:44:57 +0800

    style: 搜索框图标改为 lucide Search

--- frontend/src/pages/status/StatusPage.tsx
+++ frontend/src/pages/status/StatusPage.tsx
@@ -12,7 +12,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import { useTheme } from '../../contexts/ThemeContext';
 import LanguageSelector from "../../components/LanguageSelector";
 import { SunIcon, MoonIcon } from '@radix-ui/react-icons';
-import { LayoutGrid, Rows3, List, Server, Activity, CheckCircle, XCircle, Box, Globe, ArrowUp } from 'lucide-react';
+import { LayoutGrid, Rows3, List, Server, Activity, CheckCircle, XCircle, Box, Globe, ArrowUp, Search } from 'lucide-react';
 import { ENV_API_BASE_URL } from '../../config';
 import { useTranslation } from 'react-i18next';
 
@@ -219,7 +219,7 @@ const StatusPage = () => {
         {/* Search bar */}
         <div className="mb-4 relative">
           <span className="absolute left-3 top-1/2 -translate-y-1/2 text-slate-400">
-            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"/></svg>
+            <Search size={16} />
           </span>
           <input type="text" value={search} onChange={e => setSearch(e.target.value)}
             placeholder="搜索服务器、API服务、标签..."
